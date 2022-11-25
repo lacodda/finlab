d870cd9e428a9845a81f4b195064a424fedab2b1
Kirill Lakhtachev <lahtachev@gmail.com>
2022-11-25 21:13:52 +0100

    update api auth controller

--- apps/api/src/app/controllers/auth.controller.ts
+++ apps/api/src/app/controllers/auth.controller.ts
@@ -1,19 +1,32 @@
 import { AccountLogin, AccountRegister } from '@finlab/contracts';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { RMQService } from 'nestjs-rmq';
+import { LoginDto } from '../dtos/login.dto';
+import { RegisterDto } from '../dtos/register.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private readonly rmqService: RMQService) {}
 
   @Post('register')
-  async register(@Body() dto: AccountRegister.Request): Promise<AccountRegister.Response> {
-    return await this.authService.register(dto);
+  async register(@Body() dto: RegisterDto): Promise<AccountRegister.Response> {
+    try {
+      return await this.rmqService.send<AccountRegister.Request, AccountRegister.Response>(AccountRegister.topic, dto);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
   }
 
   @Post('login')
-  async login(@Body() dto: AccountLogin.Request): Promise<AccountLogin.Response> {
-    const { _id } = await this.authService.validateUser(dto);
-    // eslint-disable-next-line @typescript-eslint/return-await
-    return this.authService.login(_id);
+  async login(@Body() dto: LoginDto): Promise<AccountLogin.Response> {
+    try {
+      return await this.rmqService.send<AccountLogin.Request, AccountLogin.Response>(AccountLogin.topic, dto);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
   }
 }
