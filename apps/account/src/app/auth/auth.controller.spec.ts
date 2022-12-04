ebdc60f3fe7cf5ecef14c2c687fefafaf30625c0
Kirill Lakhtachev <lahtachev@gmail.com>
2022-12-04 21:14:37 +0100

    account: update test auth.controller.spec

--- apps/account/src/app/auth/auth.controller.spec.ts
+++ apps/account/src/app/auth/auth.controller.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { RMQModule, RMQService, RMQTestService } from 'nestjs-rmq';
+import { getMongoConfig } from '../configs/mongo.config';
+import { INestApplication } from '@nestjs/common';
+import { UserRepository } from '../user/repositories/user.repository';
+import { AccountLogin, AccountRegister } from '@finlab/contracts';
+
+const authLogin: AccountLogin.Request = {
+  email: 'admin2@mail.com',
+  password: '1'
+};
+const authRegister: AccountRegister.Request = {
+  ...authLogin,
+  displayName: 'Admin2'
+};
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let app: INestApplication;
+  let userRepository: UserRepository;
+  let rmqService: RMQTestService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController]
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true, envFilePath: 'envs/.account.env' }),
+        MongooseModule.forRootAsync(getMongoConfig()),
+        RMQModule.forTest({}),
+        UserModule,
+        AuthModule
+      ]
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    app = module.createNestApplication();
+    userRepository = app.get<UserRepository>(UserRepository);
+    rmqService = app.get(RMQService);
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Register', async () => {
+    const res = await rmqService.triggerRoute<AccountRegister.Request, AccountRegister.Response>(AccountRegister.topic, authRegister);
+    expect(res.email).toEqual(authRegister.email);
+  });
+
+  it('Login', async () => {
+    const res = await rmqService.triggerRoute<AccountLogin.Request, AccountLogin.Response>(AccountLogin.topic, authLogin);
+    expect(res.access_token).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await userRepository.deleteUser(authRegister.email);
+    void app.close();
   });
 });
