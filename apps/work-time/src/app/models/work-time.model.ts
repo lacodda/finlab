bf00bee0753ac5dd91681def43a459afaa072102
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-09 16:46:13 +0100

    work-time: add model

---
+++ apps/work-time/src/app/models/work-time.model.ts
@@ -0,0 +1,17 @@
+import { Document } from 'mongoose';
+import { IWorkTime } from '@finlab/interfaces';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class WorkTime extends Document implements IWorkTime {
+  @Prop({ required: true })
+    userId: string;
+
+  @Prop({ required: true })
+    date: Date;
+
+  @Prop({ required: true })
+    time: number;
+}
+
+export const WorkTimeSchema = SchemaFactory.createForClass(WorkTime);
