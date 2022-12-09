acf779b8df09eb2d098db706784ce3f3127b5642
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-09 19:28:25 +0100

    work-time: add entity and repository

--- apps/work-time/src/app/app.module.ts
+++ apps/work-time/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { RMQModule } from 'nestjs-rmq';
+import { getMongoConfig } from './configs/mongo.config';
+import { getRmqConfig } from './configs/rmq.config';
+import { WorkTime, WorkTimeSchema } from './models/work-time.model';
+import { WorkTimeRepository } from './repositories/work-time.repository';
 import { WorkTimeController } from './work-time.controller';
 import { WorkTimeService } from './work-time.service';
-import { WorkTime, WorkTimeSchema } from './work-time.schema';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: 'envs/.work-time.env', isGlobal: true }),
+    RMQModule.forRootAsync(getRmqConfig()),
+    MongooseModule.forRootAsync(getMongoConfig()),
     MongooseModule.forFeature([
       { name: WorkTime.name, schema: WorkTimeSchema }
     ])
   ],
   controllers: [WorkTimeController],
-  providers: [WorkTimeService],
-  exports: [WorkTimeService]
+  providers: [WorkTimeService, WorkTimeRepository],
+  exports: [WorkTimeService, WorkTimeRepository]
 })
 export class AppModule {}
