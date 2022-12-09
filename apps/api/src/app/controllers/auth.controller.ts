9fb5ee4919b086cb90ec535388b6fd18357e0d6b
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-09 12:40:29 +0100

    api: removed dtos

--- apps/api/src/app/controllers/auth.controller.ts
+++ apps/api/src/app/controllers/auth.controller.ts
@@ -1,15 +1,13 @@
 import { AccountLogin, AccountRegister } from '@finlab/contracts';
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { RMQService } from 'nestjs-rmq';
-import { LoginDto } from '../dtos/login.dto';
-import { RegisterDto } from '../dtos/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly rmqService: RMQService) {}
 
   @Post('register')
-  async register(@Body() dto: RegisterDto): Promise<AccountRegister.Response> {
+  async register(@Body() dto: AccountRegister.Request): Promise<AccountRegister.Response> {
     try {
       return await this.rmqService.send<AccountRegister.Request, AccountRegister.Response>(AccountRegister.topic, dto);
     } catch (error) {
@@ -20,7 +18,7 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() dto: LoginDto): Promise<AccountLogin.Response> {
+  async login(@Body() dto: AccountLogin.Request): Promise<AccountLogin.Response> {
     try {
       return await this.rmqService.send<AccountLogin.Request, AccountLogin.Response>(AccountLogin.topic, dto);
     } catch (error) {
