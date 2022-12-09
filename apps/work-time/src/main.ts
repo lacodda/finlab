77da4ee842bf82b86c8fc31e776ecd7d9a794157
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-09 16:45:51 +0100

    work-time: update main.ts

--- apps/work-time/src/main.ts
+++ apps/work-time/src/main.ts
@@ -1,22 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
 
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-
-import { AppModule } from "./app/app.module";
-
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await app.init();
+  Logger.log('🚀 Work Time is running');
 }
 
-bootstrap();
+void bootstrap();
