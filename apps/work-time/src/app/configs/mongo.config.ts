e96f974ef0fdeb49f61e2f7c185a3f68c96ddacb
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-09 16:43:01 +0100

    work-time: add configs

---
+++ apps/work-time/src/app/configs/mongo.config.ts
@@ -0,0 +1,20 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModuleAsyncOptions } from '@nestjs/mongoose';
+
+export const getMongoConfig = (): MongooseModuleAsyncOptions => {
+  return {
+    useFactory: (configService: ConfigService) => ({
+      uri: getMongoString(configService)
+    }),
+    inject: [ConfigService],
+    imports: [ConfigModule]
+  };
+};
+
+const getMongoString = (configService: ConfigService): string =>
+  'mongodb://' + String(configService.get('MONGO_LOGIN')) +
+  ':' + String(configService.get('MONGO_PASSWORD')) +
+  '@' + String(configService.get('MONGO_HOST')) +
+  ':' + String(configService.get('MONGO_PORT')) +
+  '/' + String(configService.get('MONGO_DATABASE')) +
+  '?authSource=' + String(configService.get('MONGO_AUTHDATABASE'));
---
+++ apps/work-time/src/app/configs/rmq.config.ts
@@ -0,0 +1,20 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { IRMQServiceAsyncOptions } from 'nestjs-rmq';
+
+export const getRmqConfig = (): IRMQServiceAsyncOptions => ({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    exchangeName: configService.get('AMQP_EXCHANGE') ?? '',
+    connections: [
+      {
+        login: configService.get('AMQP_USER') ?? '',
+        password: configService.get('AMQP_PASSWORD') ?? '',
+        host: configService.get('AMQP_HOST') ?? ''
+      }
+    ],
+    queueName: configService.get('AMQP_QUEUE'),
+    prefetchCount: 32,
+    serviceName: 'finlab-work-time'
+  })
+});
