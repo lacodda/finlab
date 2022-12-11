e70c44de14c04df5af24244581454d9db843ebe5
Kirill Lakhtachev <lahtachev@gmail.com>
2022-12-11 15:58:59 +0100

    frontend: lint

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import React from 'react';
 import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function CustomApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <title>Welcome to frontend!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps}/>
+        <Component {...pageProps} />
       </main>
     </>
   );
