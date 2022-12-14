7caf5491378adec0282cc0a77b3706723dd7d9d1
Kirill Lakhtachev <lahtachev@gmail.com>
2022-12-14 22:23:02 +0100

    frontend: add tailwind

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,13 +1,13 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
-import './styles.css';
+import '../styles/globals.scss';
 
 function CustomApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Head>
-        <title>Welcome to frontend!</title>
+        <title>finlab</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
