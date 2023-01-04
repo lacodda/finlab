4ccc18749944933a5129bf658335f5e625faa405
Kirill Lakhtachev <lahtachev@gmail.com>
2023-01-04 22:02:04 +0100

    frontend: add google font

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,12 +1,19 @@
 
 import React from 'react';
+import { Button, Input } from '../components';
 import styles from './index.module.scss';
+import { BeakerIcon } from '@heroicons/react/24/outline';
 
 export function Index(): JSX.Element {
   return (
-    <h1 className="text-3xl font-bold underline">
-      finlab
-    </h1>
+    <>
+    <h1 className="text-3xl font-bold underline">Заголовок</h1>
+    <Button>Button <BeakerIcon className="h-4 w-4 ml-1"/></Button>
+    <Button rounded={true}>Button <BeakerIcon className="h-4 w-4 ml-1"/></Button>
+    <Button size='s'>Button <BeakerIcon className="h-4 w-4 ml-1"/></Button>
+    <Button size='l'>Button <BeakerIcon className="h-4 w-4 ml-1"/></Button>
+    <Input/>
+    </>
   );
 }
 
