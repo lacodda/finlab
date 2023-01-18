7510de9f91fb36a9ba348dcd70dd455ebad31771
Kirill Lakhtachev <lahtachev@gmail.com>
2023-01-18 16:42:37 +0100

    libs: add helpers

---
+++ libs/helpers/src/lib/time.helper.ts
@@ -0,0 +1,26 @@
+import { IDateRange, IDateRangeISO } from '@finlab/interfaces';
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class Time {
+  public static dayRange(dayISO?: string): IDateRange {
+    const date = dayISO ? new Date(dayISO) : new Date();
+    return {
+      from: new Date(date.setUTCHours(0, 0, 0, 0)),
+      to: new Date(date.setUTCHours(23, 59, 59, 999))
+    };
+  }
+
+  public static dayRangeISO(dayISO?: string): IDateRangeISO {
+    const dayRange = Time.dayRange(dayISO);
+    return {
+      from: dayRange.from.toISOString(),
+      to: dayRange.to.toISOString()
+    };
+  }
+
+  public static diffInMinutes(from: Date, to: Date): number {
+    let diff = (from.getTime() - to.getTime()) / 1000;
+    diff /= 60;
+    return Math.abs(Math.round(diff));
+  }
+}
