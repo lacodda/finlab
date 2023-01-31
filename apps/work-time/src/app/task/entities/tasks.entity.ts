6ad9a1d780e7cb727c556e77662478e04f278f1c
Kirill Lakhtachev <lahtachev@gmail.com>
2023-01-31 13:35:14 +0100

    work-time: add tasks entity

---
+++ apps/work-time/src/app/task/entities/tasks.entity.ts
@@ -0,0 +1,23 @@
+import { ITask, ITaskFindIncompleteResult } from '@finlab/interfaces';
+import { TaskEntity } from './task.entity';
+
+export class TasksEntity {
+  tasks: TaskEntity[] = [];
+
+  constructor(tasks: Array<ITask | ITaskFindIncompleteResult>) {
+    this.tasks = tasks.map(task => {
+      if (this.instanceOfITaskFindIncompleteResult(task)) {
+        return new TaskEntity(task.tasks.find(({ completeness }) => completeness === task.completeness) as ITask);
+      }
+      return new TaskEntity(task);
+    });
+  }
+
+  private instanceOfITaskFindIncompleteResult(task: ITask | ITaskFindIncompleteResult): task is ITaskFindIncompleteResult {
+    return 'tasks' in task;
+  }
+
+  public get entities(): Array<Omit<ITask, 'userId'>> {
+    return this.tasks.map(({ entity }) => entity);
+  }
+}
