c43fd4cb4ae5109d1fe001b1323e7d08b5bcc0ea
Kirill Lakhtachev <lahtachev@gmail.com>
2023-02-19 16:18:20 +0100

    api: added method recalculateSummary

--- apps/api/src/app/controllers/work-time.controller.ts
+++ apps/api/src/app/controllers/work-time.controller.ts
@@ -1,6 +1,6 @@
 import { Get, Param, Delete, Patch, Query, Body, Controller, Post, BadRequestException, UseGuards } from '@nestjs/common';
 import {
-  SummaryCreate, SummaryGetByQuery, SummaryGetById, SummaryUpdate, SummaryDelete,
+  SummaryCreate, SummaryGetByQuery, SummaryGetById, SummaryUpdate, SummaryDelete, SummaryRecalculate,
   TaskCreate, TaskDelete, TaskGetById, TaskGetByQuery, TaskUpdate,
   TimestampCreate, TimestampDelete, TimestampGetById, TimestampGetByQuery, TimestampUpdate
 } from '@finlab/contracts/work-time';
@@ -157,7 +157,7 @@ export class WorkTimeController {
   @ApiTags('summary')
   @UseGuards(JwtAuthGuard)
   @Post('summary')
-  async create(@Body() dto: Omit<SummaryCreate.Request, 'userId'>, @UserId() userId: string): Promise<SummaryCreate.Response | undefined> {
+  async createSummary(@Body() dto: Omit<SummaryCreate.Request, 'userId'>, @UserId() userId: string): Promise<SummaryCreate.Response | undefined> {
     try {
       return await this.rmqService.send<SummaryCreate.Request, SummaryCreate.Response>(SummaryCreate.topic, { ...dto, userId });
     } catch (error) {
@@ -170,7 +170,7 @@ export class WorkTimeController {
   @ApiTags('summary')
   @UseGuards(JwtAuthGuard)
   @Patch('summary/:id')
-  async update(@Param('id') id: string, @Body() dto: Omit<SummaryUpdate.Request, 'id'>): Promise<SummaryUpdate.Response | undefined> {
+  async updateSummary(@Param('id') id: string, @Body() dto: Omit<SummaryUpdate.Request, 'id'>): Promise<SummaryUpdate.Response | undefined> {
     try {
       return await this.rmqService.send<SummaryUpdate.Request, SummaryUpdate.Response>(SummaryUpdate.topic, { ...dto, id });
     } catch (error) {
@@ -182,8 +182,21 @@ export class WorkTimeController {
 
   @ApiTags('summary')
   @UseGuards(JwtAuthGuard)
+  @Get('summary/recalculate')
+  async recalculateSummary(@Query() dto: Omit<SummaryRecalculate.Request, 'userId'>, @UserId() userId: string): Promise<SummaryRecalculate.Response | undefined> {
+    try {
+      return await this.rmqService.send<SummaryRecalculate.Request, SummaryRecalculate.Response>(SummaryRecalculate.topic, { ...dto, userId });
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new BadRequestException(error.message);
+      }
+    }
+  }
+
+  @ApiTags('summary')
+  @UseGuards(JwtAuthGuard)
   @Get('summary')
-  async getByQuery(@Query() dto: Omit<SummaryGetByQuery.Request, 'userId'>, @UserId() userId: string): Promise<SummaryGetByQuery.Response | undefined> {
+  async getByQuerySummary(@Query() dto: Omit<SummaryGetByQuery.Request, 'userId'>, @UserId() userId: string): Promise<SummaryGetByQuery.Response | undefined> {
     try {
       return await this.rmqService.send<SummaryGetByQuery.Request, SummaryGetByQuery.Response>(SummaryGetByQuery.topic, { ...dto, userId });
     } catch (error) {
@@ -196,7 +209,7 @@ export class WorkTimeController {
   @ApiTags('summary')
   @UseGuards(JwtAuthGuard)
   @Get('summary/:id')
-  async getById(@Param('id') id: string): Promise<SummaryGetById.Response | undefined> {
+  async getByIdSummary(@Param('id') id: string): Promise<SummaryGetById.Response | undefined> {
     try {
       return await this.rmqService.send<SummaryGetById.Request, SummaryGetById.Response>(SummaryGetById.topic, { id });
     } catch (error) {
@@ -209,7 +222,7 @@ export class WorkTimeController {
   @ApiTags('summary')
   @UseGuards(JwtAuthGuard)
   @Delete('summary/:id')
-  async delete(@Param('id') id: string): Promise<SummaryDelete.Response | undefined> {
+  async deleteSummary(@Param('id') id: string): Promise<SummaryDelete.Response | undefined> {
     try {
       return await this.rmqService.send<SummaryDelete.Request, SummaryDelete.Response>(SummaryDelete.topic, { id });
     } catch (error) {
