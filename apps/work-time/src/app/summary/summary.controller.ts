ed11afc61cb02adb9674153e28ce6838f89a3235
Kirill Lakhtachev <lahtachev@gmail.com>
2023-03-09 20:33:13 +0100

    work-time: updated summary service

--- apps/work-time/src/app/summary/summary.controller.ts
+++ apps/work-time/src/app/summary/summary.controller.ts
@@ -1,10 +1,4 @@
-import {
-  type SummaryCreateResponse, SummaryCreateTopic, SummaryCreateUserIdRequest, type SummaryDeleteResponse, SummaryDeleteTopic,
-  SummaryDeleteUserIdRequest, type SummaryGetByQueryResponse, SummaryGetByQueryTopic, SummaryGetByQueryUserIdRequest,
-  type SummaryGetOneResponse, SummaryGetOneTopic, SummaryGetOneUserIdRequest, type SummaryRecalculateResponse, SummaryRecalculateTopic,
-  SummaryRecalculateUserIdRequest, type SummaryUpdateResponse, SummaryUpdateTopic, SummaryUpdateUserIdRequest, TimestampChangedTotalTimeTopic,
-  TimestampChangedTotalTimeUserIdRequest
-} from '@finlab/contracts/work-time';
+import { type SummaryGetResponse, SummaryGetTopic, SummaryGetUserIdRequest, TimestampChangedTotalTimeTopic, TimestampChangedTotalTimeUserIdRequest } from '@finlab/contracts/work-time';
 import { Body, Controller } from '@nestjs/common';
 import { RMQRoute, RMQValidate } from 'nestjs-rmq';
 import { SummaryService } from './summary.service';
@@ -14,44 +8,14 @@ export class SummaryController {
   constructor(private readonly summaryService: SummaryService) {}
 
   @RMQValidate()
-  @RMQRoute(SummaryCreateTopic)
-  async create(@Body() dto: SummaryCreateUserIdRequest): Promise<SummaryCreateResponse> {
-    return await this.summaryService.create(dto);
-  }
-
-  @RMQValidate()
-  @RMQRoute(SummaryUpdateTopic)
-  async update(@Body() dto: SummaryUpdateUserIdRequest): Promise<SummaryUpdateResponse> {
-    return await this.summaryService.update(dto);
-  }
-
-  @RMQValidate()
-  @RMQRoute(SummaryGetByQueryTopic)
-  async getByQuery(@Body() dto: SummaryGetByQueryUserIdRequest): Promise<SummaryGetByQueryResponse> {
+  @RMQRoute(SummaryGetTopic)
+  async getByQuery(@Body() dto: SummaryGetUserIdRequest): Promise<SummaryGetResponse> {
     return await this.summaryService.getByQuery(dto);
-  }
-
-  @RMQValidate()
-  @RMQRoute(SummaryGetOneTopic)
-  async getOne(@Body() dto: SummaryGetOneUserIdRequest): Promise<SummaryGetOneResponse> {
-    return await this.summaryService.getOne(dto);
-  }
-
-  @RMQValidate()
-  @RMQRoute(SummaryDeleteTopic)
-  async delete(@Body() dto: SummaryDeleteUserIdRequest): Promise<SummaryDeleteResponse> {
-    return await this.summaryService.delete(dto);
   }
 
   @RMQValidate()
   @RMQRoute(TimestampChangedTotalTimeTopic)
   async timestampChanged(@Body() dto: TimestampChangedTotalTimeUserIdRequest): Promise<void> {
     await this.summaryService.create(dto);
-  }
-
-  @RMQValidate()
-  @RMQRoute(SummaryRecalculateTopic)
-  async recalculate(@Body() dto: SummaryRecalculateUserIdRequest): Promise<SummaryRecalculateResponse> {
-    return await this.summaryService.recalculate(dto);
   }
 }
--- apps/work-time/src/app/summary/summary.service.ts
+++ apps/work-time/src/app/summary/summary.service.ts
@@ -1,13 +1,7 @@
-import {
-  type SummaryCreateResponse, type SummaryCreateUserIdRequest, type SummaryDeleteResponse, type SummaryDeleteUserIdRequest,
-  type SummaryGetByQueryResponse, SummaryGetByQueryTopic, type SummaryGetByQueryUserIdRequest, type SummaryGetOneResponse,
-  type SummaryGetOneUserIdRequest, type SummaryRecalculateResponse, type SummaryRecalculateUserIdRequest, type SummaryUpdateResponse,
-  type SummaryUpdateUserIdRequest, type TimestampGetByQueryResponse, type TimestampGetByQueryUserIdRequest
-} from '@finlab/contracts/work-time';
+import { type SummaryGetResponse, type SummaryGetUserIdRequest, type TimestampGetResponse, type TimestampGetUserIdRequest, TimestampGetTopic, type SummaryUserId } from '@finlab/contracts/work-time';
 import { Time } from '@finlab/helpers';
-import { type ISummaryFindByQueryParams, type ISummary } from '@finlab/interfaces/work-time';
+import { type ISummaryFindByQueryParams } from '@finlab/interfaces/work-time';
 import { Injectable } from '@nestjs/common';
-import { type UpdateWriteOpResult } from 'mongoose';
 import { RMQService } from 'nestjs-rmq';
 import { SummaryEntity } from './entities/summary.entity';
 import { SummaryRepository } from './repositories/summary.repository';
@@ -18,47 +12,33 @@ export class SummaryService {
     private readonly summaryRepository: SummaryRepository,
     private readonly rmqService: RMQService) { }
 
-  async create(dto: SummaryCreateUserIdRequest): Promise<SummaryCreateResponse> {
-    const date = Time.dayRange(dto.date).from;
-    const existedSummary = await this.summaryRepository.findByDate(date, dto.userId);
-    if (existedSummary) {
-      const data = await this.updateTime(existedSummary, dto.time);
-      return { data };
-    }
-    const newSummaryEntity = new SummaryEntity({ ...dto, date });
-    const newSummary = await this.summaryRepository.create(newSummaryEntity);
-
-    return { data: new SummaryEntity(newSummary).entity };
-  }
-
-  async update(dto: SummaryUpdateUserIdRequest): Promise<SummaryUpdateResponse> {
+  async create(dto: SummaryUserId): Promise<void> {
     const existedSummary = await this.summaryRepository.findByDate(dto.date, dto.userId);
-    if (!existedSummary) {
-      throw new Error('Unable to update non-existing entry');
+    if (existedSummary) {
+      const summaryEntity = new SummaryEntity(existedSummary).updateTime(dto.time);
+      await this.summaryRepository.update(summaryEntity);
+      return;
     }
-    const summaryEntity = new SummaryEntity(existedSummary).updateTime(dto.time);
-    await this.updateSummary(summaryEntity);
 
-    return { data: summaryEntity.entity };
+    const summaryEntity = new SummaryEntity(dto);
+    await this.summaryRepository.create(summaryEntity);
   }
 
-  async updateTime(summary: ISummary, time: number): Promise<Omit<ISummary, 'userId'>> {
-    const summaryEntity = new SummaryEntity(summary).updateTime(time);
-    await this.summaryRepository.update(summaryEntity);
-    return summaryEntity.entity;
-  }
-
-  async getByQuery(dto: SummaryGetByQueryUserIdRequest): Promise<SummaryGetByQueryResponse> {
+  async getByQuery(dto: SummaryGetUserIdRequest): Promise<SummaryGetResponse> {
+    const { from, to } = Time.monthRange();
     const params: ISummaryFindByQueryParams = {
-      userId: dto.userId
+      userId: dto.userId,
+      date: {
+        $gte: dto.from ? Time.dayRange(dto.from).from : from,
+        $lte: dto.to ? Time.dayRange(dto.to).to : to
+      }
     };
-    if (dto.from && dto.to) {
-      params.date = {
-        $gte: new Date(dto.from),
-        $lte: new Date(dto.to)
-      };
+
+    if (dto.recalculate) {
+      await this.recalculate(params);
     }
-    const { data, totalTime } = await this.summaryRepository.findByQuery(params);
+
+    const { data, totalTime } = await this.summaryRepository.findByQuery(params) ?? { data: [], totalTime: 0 };
 
     return {
       data: data.map(summary => new SummaryEntity(summary).entity),
@@ -66,47 +46,22 @@ export class SummaryService {
     };
   }
 
-  async getOne(dto: SummaryGetOneUserIdRequest): Promise<SummaryGetOneResponse> {
-    const existedSummary = await this.summaryRepository.findByDate(dto.date, dto.userId);
-    if (!existedSummary) {
-      throw new Error('Unable to delete non-existing entry');
-    }
-
-    return { data: new SummaryEntity(existedSummary).entity };
-  }
-
-  async recalculate({ userId, from, to }: SummaryRecalculateUserIdRequest): Promise<SummaryRecalculateResponse> {
-    const dates = Time.datesInRange(from, to);
-    for (const dateObj of dates) {
-      const date = dateObj;
-      const { totalTime: time } = await this.rmqService.send<TimestampGetByQueryUserIdRequest, TimestampGetByQueryResponse>(
-        SummaryGetByQueryTopic, { userId, date }
+  private async recalculate({ userId, date }: ISummaryFindByQueryParams): Promise<void> {
+    const dates = Time.datesInRange(date.$gte, date.$lte);
+    for (const date of dates) {
+      const { totalTime: time } = await this.rmqService.send<TimestampGetUserIdRequest, TimestampGetResponse>(
+        TimestampGetTopic, { userId, date }
       );
-      if (time) {
-        await this.create({ userId, date, time });
-      } else {
-        await this.delete({ userId, date });
+
+      if (!time) {
+        const existedSummary = await this.summaryRepository.findByDate(date, userId);
+        if (existedSummary) {
+          await this.summaryRepository.delete(existedSummary._id as string);
+        }
+        return;
       }
+
+      await this.create({ userId, date, time });
     }
-
-    return await this.getByQuery({ userId, from, to });
-  }
-
-  async delete(dto: SummaryDeleteUserIdRequest): Promise<SummaryDeleteResponse> {
-    const dayRange = Time.dayRange(dto.date);
-    const existedSummary = await this.summaryRepository.findByDate(dayRange.from, dto.userId);
-    if (existedSummary) {
-      await this.summaryRepository.delete(existedSummary._id as string);
-    }
-
-    return { data: existedSummary };
-  }
-
-  private async updateSummary(summary: SummaryEntity): Promise<[UpdateWriteOpResult]> {
-    return await Promise.all(
-      [
-        this.summaryRepository.update(summary)
-      ]
-    );
   }
 }
