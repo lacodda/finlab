416c13f99df0b9b2cc416bd7c4b4096e2ced21ee
Kirill Lakhtachev <lahtachev@gmail.com>
2023-03-10 20:29:50 +0100

    libs: updated contracts for work-time summary

--- libs/contracts/src/lib/work-time/summary/summary.get.ts
+++ libs/contracts/src/lib/work-time/summary/summary.get.ts
@@ -4,23 +4,27 @@ import { ArgsType, Field, ObjectType } from '@nestjs/graphql';
 import { type UserId } from '../../common';
 import { Summary } from './summary.model';
 import { ToBoolean } from '../../decorators';
+import { ApiProperty } from '@nestjs/swagger';
 
 export const SummaryGetTopic = 'work-time.summary.get.query';
 
 @ArgsType()
 export class SummaryGetRequest {
+  @ApiProperty()
   @IsOptional()
   @IsDate()
   @Field({ nullable: true })
   @Type(() => Date)
     from?: Date;
 
+  @ApiProperty()
   @IsOptional()
   @IsDate()
   @Field({ nullable: true })
   @Type(() => Date)
     to?: Date;
 
+  @ApiProperty()
   @IsOptional()
   @IsBoolean()
   @Field(() => Boolean, { nullable: true })
@@ -33,9 +37,11 @@ export interface SummaryGetUserIdRequest extends UserId, SummaryGetRequest { }
 
 @ObjectType()
 export class SummaryGetResponse {
+  @ApiProperty({ type: [Summary] })
   @Field(() => [Summary])
     data: Summary[];
 
+  @ApiProperty()
   @Field()
     totalTime: number;
 }
--- libs/contracts/src/lib/work-time/summary/summary.model.ts
+++ libs/contracts/src/lib/work-time/summary/summary.model.ts
@@ -1,12 +1,15 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { type ISummary } from '@finlab/interfaces/work-time';
 import { type UserId } from '../../common';
+import { ApiProperty } from '@nestjs/swagger';
 
-@ObjectType({ description: 'timestamp ' })
+@ObjectType({ description: 'Summary' })
 export class Summary implements Omit<ISummary, 'userId'> {
+  @ApiProperty()
   @Field({ nullable: true })
     date: Date;
 
+  @ApiProperty()
   @Field(() => Number, { nullable: true })
     time: number;
 }
