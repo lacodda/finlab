b0769b65112216aed7d0db34c019d11e795c89c4
Kirill Lakhtachev <lahtachev@gmail.com>
2023-03-21 14:38:22 +0100

    graphql: deleted awagger config

--- apps/graphql/src/app/configs/swagger.config.ts
+++
@@ -1,7 +0,0 @@
-import { DocumentBuilder, type OpenAPIObject } from '@nestjs/swagger';
-
-export const getSwaggerConfig = (): Omit<OpenAPIObject, 'paths'> => new DocumentBuilder()
-  .setTitle('finlab')
-  .setDescription('finlab REST API')
-  .setVersion('1.0')
-  .build();
