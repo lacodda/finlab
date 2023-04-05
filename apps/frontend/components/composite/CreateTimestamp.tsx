01a0dddd411ea0148052aaa6909e31a02a4b50fe
Kirill Lakhtachev <lahtachev@gmail.com>
2023-04-05 16:26:43 +0200

    frontend: updated timestamp coponents

--- apps/frontend/components/composite/CreateTimestamp.tsx
+++ apps/frontend/components/composite/CreateTimestamp.tsx
@@ -1,37 +1,37 @@
 
-import React, { type DetailedHTMLProps, type HTMLAttributes, useState, useCallback, useEffect } from 'react';
+import React, { type DetailedHTMLProps, type HTMLAttributes, useState, useEffect } from 'react';
 import cn from 'classnames';
-import { FinlabApi, type ITimestamp, TimestampType } from '../../graphql';
+import { TimestampType } from '../../graphql';
 import { Button } from '../elementary/Button';
 import { Input } from '../elementary/Input';
+import { Timestamp } from '../../entities';
 
 export interface CreateTimestampProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  value?: ITimestamp;
+  value?: Timestamp;
 }
 
 export const CreateTimestamp = ({ className, value, ...props }: CreateTimestampProps): JSX.Element => {
-  const [timestamp, setTimestamp] = useState<string>('');
+  const [timestampStr, setTimestampStr] = useState<string>('');
   const [type, setType] = useState<TimestampType>(TimestampType.Start);
+  const [timestamp, setTimestamp] = useState<Timestamp>(new Timestamp());
+
+  async function save(): Promise<void> {
+    await new Timestamp().setTimestamp(timestampStr).setType(type).save();
+  }
 
   useEffect(() => {
-    if (!value) {
-      return;
-    }
-    setTimestamp(value.timestamp);
-    setType(value.type);
+    setTimestamp(new Timestamp(value));
+    setTimestampStr(timestamp.timestamp.toISOString());
+    setType(timestamp.type);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value]);
-
-  const timestampCreate = FinlabApi.workTime.timestamp.Create();
-  const Save = useCallback(() => {
-    void timestampCreate.exec({ timestamp: new Date(timestamp), type });
-  }, [timestamp, timestampCreate, type]);
 
   return (
     <div className={cn(className)}>
       <form>
-        <Input type='timestamp' placeholder='Timestamp' label='Timestamp' value={timestamp} onChange={(e) => { setTimestamp(e.target.value); }} />
+        <Input type='timestamp' placeholder='Timestamp' label='Timestamp' value={timestampStr} onChange={(e) => { setTimestampStr(e.target.value); }} />
         <Input type='type' placeholder='Type' label='Type' value={type} onChange={(e) => { setType(e.target.value as TimestampType); }} />
-        <Button className='mt-6 w-full justify-center font-bold uppercase' onClick={Save}>Save</Button>
+        <Button className='mt-6 w-full justify-center font-bold uppercase' onClick={() => { void save(); }}>Save</Button>
       </form>
     </div>
   );
--- apps/frontend/components/composite/UserProfile.tsx
+++ apps/frontend/components/composite/UserProfile.tsx
@@ -1,36 +1,39 @@
 
 import React, { type DetailedHTMLProps, type HTMLAttributes, useEffect, useState } from 'react';
 import cn from 'classnames';
-import { FinlabApi, type ITimestamp } from '../../graphql';
 import { CreateTimestamp } from './CreateTimestamp';
 import { Button } from '../elementary/Button';
+import { type Timestamp, Timestamps } from '../../entities';
 
 export interface UserProfileProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
 
 export const UserProfile = ({ className, ...props }: UserProfileProps): JSX.Element => {
-  const timestamps = FinlabApi.workTime.timestamp.Get();
+  const [timestamp, setTimestamp] = useState<Timestamp>();
+  const [timestampList, setTimestampList] = useState<Timestamp[]>([]);
+  const [timestampError, setTimestampError] = useState<Error>();
+  const timestamps = new Timestamps();
+
+  async function getTimestamps(): Promise<void> {
+    await timestamps.fetch({ raw: true });
+    setTimestampList(timestamps.timestampList);
+    setTimestampError(timestamps.error);
+  }
+
   useEffect(() => {
-    void timestamps.exec({ raw: true });
+    void getTimestamps();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const [timestamp, setTimestamp] = useState<ITimestamp>();
-
-  const timestampDelete = FinlabApi.workTime.timestamp.Delete();
-  const Delete = (e: React.MouseEvent, timestamp: string): void => {
-    e.stopPropagation();
-    void timestampDelete.exec({ timestamp: new Date(timestamp) });
-  };
-
   return (
     <div className={cn(className)}>
-      {timestamps.error && <p>There is an error!</p>}
-      {timestamps.loading && <p>Loading...</p>}
+      {timestampError && <p>There is an error!</p>}
+      {/* {timestamps.loading && <p>Loading...</p>} */}
+      <Button className='w-full justify-center font-bold uppercase' onClick={() => { void getTimestamps(); }}>Ok</Button>
       <div className='grid grid-cols-3 justify-center items-center gap-4'>
-        {timestamps.data?.timestamps.data.map((i, k) => <div key={k} className='contents cursor-pointer' onClick={() => { setTimestamp(i); }}>
-          <div>{i.timestamp}</div>
-          <div>{i.type}</div>
-          <Button className='w-full justify-center font-bold uppercase' onClick={(e) => { Delete(e, i.timestamp); }}>Delete</Button>
+        {timestampList.map((item, k) => <div key={k} className='contents cursor-pointer' onClick={() => { setTimestamp(item); }}>
+          <div>{item.timestamp.toISOString()}</div>
+          <div>{item.type}</div>
+          <Button className='w-full justify-center font-bold uppercase' onClick={() => { void item.delete(); }}>Delete</Button>
         </div>)}
       </div>
       <CreateTimestamp value={timestamp} />
